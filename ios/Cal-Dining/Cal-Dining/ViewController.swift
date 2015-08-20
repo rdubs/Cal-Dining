23ee6ca7238f2da12626025871bdc58283439623
Ranit Dubey <rdubey@ifwe.co>
2015-08-20 14:17:56 -0700

    loaded all data in 1 network request

--- ios/Cal-Dining/Cal-Dining/ViewController.swift
+++ ios/Cal-Dining/Cal-Dining/ViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 import Parse
 
 class ViewController: UIViewController {
-    var entreesByDiningHall =  [String: [String: [PFObject]]]()
+//    var entreesByDiningHall =  [String: [String: [PFObject]]]()
+    var entreesByDiningHall = ["crossroads" : [String: [PFObject]](),
+                                            "cafe3": [String: [PFObject]](),
+                                            "foothill": [String: [PFObject]](),
+                                            "ckc": [String: [PFObject]]()
+    ]
+    
     var halls =  ["crossroads",
                 "cafe3",
                 "foothill",
@@ -22,37 +28,49 @@ class ViewController: UIViewController {
         "Dinner",
     ]
     
+    var entrees: [PFObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        for meal_index in 0...2 {
-            var meal = self.meals[meal_index]
-            for index in 0...3 {
-                var hall = self.halls[index]
-                var predicate = NSPredicate(format: "dining_hall = '\(hall)' AND meal_type = 'Breakfast'")
-                var query = PFQuery(className: "Entree", predicate: predicate)
-                query.findObjectsInBackgroundWithBlock {
-                    (results: [AnyObject]?, error: NSError?) -> Void in
-                    if error == nil {
-                        self.entreesByDiningHall[hall]![meal] = results as! [PFObject]
-                        print(results)
-                        print("test")
-                    }
-                }
+        var query = PFQuery(className: "Entree")
+        query.findObjectsInBackgroundWithBlock {
+            (results: [AnyObject]?, error: NSError?) -> Void in
+            if error == nil {
+                self.entrees = results as! [PFObject]
+                self.sortEntrees()
+            }
+        }
+    }
+    
+    func sortEntrees() {
+        for entree in self.entrees {
+            var meal = entree["meal_type"] as! String
+            var hall = entree["dining_hall"] as! String
+            if let var mealArray = self.entreesByDiningHall[hall]![meal] {
+                self.entreesByDiningHall[hall]![meal]!.append(entree)
+            } else {
+                self.entreesByDiningHall[hall]![meal] = []
             }
         }
     }
         
-    
-    func test() {
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+//        for meal_index in 0...2 {
+//            var meal = self.meals[meal_index]
+//            for index in 0...3 {
+//                var hall = self.halls[index]
+//                var predicate = NSPredicate(format: "dining_hall = '\(hall)' AND meal_type = 'Breakfast'")
+//                var query = PFQuery(className: "Entree", predicate: predicate)
+//                query.findObjectsInBackgroundWithBlock {
+//                    (results: [AnyObject]?, error: NSError?) -> Void in
+//                    if error == nil {
+//                        self.entreesByDiningHall[hall]![meal] = results as! [PFObject]
+//                        println(self.entreesByDiningHall)
+//                    }
+//                }
+//            }
+//        }
 }
+    
+
 
